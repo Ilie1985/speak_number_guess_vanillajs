15a2602aff2b8c7e47a36628eb7fbb39c449cb13
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-04 16:11:13 +0000

    window speech recognition

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const msgEl = document.getElementById('msg');
 const randomNum = getRandomNumber();
 
 console.log('Number:', randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
